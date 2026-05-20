343ceb6f4e5b478d1e5011c1d814b8e9a89b029e
Owusu_Boateng <owusuboat05@gmail.com>
2026-05-20 13:59:13 +0000

    vercel json file added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
 // If using Vite/CRA, place logo in /public and reference like this:
-const Logo = "src/assets/images/logo.png";
+const Logo = "./assets/images/logo.png";
 
 const navLinks = [
   { label: "Home", href: "/" },
